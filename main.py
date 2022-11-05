e635051fb4d6c9315ee77bf70422286eb028decf
Travis Bumgarner <travis.bumgarner@gmail.com>
2022-11-05 14:35:51 -0600

    added themes and optimized fps

--- main.py
+++ main.py
@@ -2,31 +2,35 @@ import pyvirtualcam
 import cv2
 from imutils.video import VideoStream
 import imutils
-from themes import gameboy
+from themes import *
 
 # These values should match your webcam. 
-WIDTH = 1920
-HEIGHT = 1080
+CAM_WIDTH = 1920
+CAM_HEIGHT = 1080
+
+OUTPUT_WIDTH = 480
+OUTPUT_HEIGHT = 270
 
 for i_src in range(0,4):
     vs = VideoStream(src=i_src).start() # Will need to find the src that matches your webcam. Unclear how to iterate on this. 
     img = vs.read()
     height = len(img)
     width = len(img[0])
-    if not height == HEIGHT and not width == WIDTH:
+    if not height == CAM_HEIGHT and not width == CAM_WIDTH:
         vs.stop()
     else:
         print(f'using index {i_src}')
         break
 
 
-with pyvirtualcam.Camera(width=WIDTH, height=HEIGHT, fps=30) as cam:
+with pyvirtualcam.Camera(width=OUTPUT_WIDTH, height=OUTPUT_HEIGHT, fps=30) as cam:
     while True:
         img = vs.read()
-        img = imutils.resize(img, width=WIDTH, height=HEIGHT)
+        img = imutils.resize(img, width=OUTPUT_WIDTH, height=OUTPUT_HEIGHT)
         img = imutils.opencv2matplotlib(img) 
 
-        img = gameboy(img)
+        # img = gameboy(img)
+        img = theme_black_and_white(img)
         
         # Webcam sends RGB but we need BGR. 
         cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
--- themes.py
+++ themes.py
@@ -1,19 +1,18 @@
 import cv2
 import numpy as np
 
-
-def gameboy(img):
-    colors = [
-        [15, 56, 15], # Darkest
-        [48, 98, 48], # Darker
-        [139, 172, 15], # Light 
-        [155, 188, 15], # Lightest
-    ]
-
+def theme_method_colors_by_brightness(img, colors, colors_repeated=1):
+    """
+    Map the array passed in of colors = [darkest, darker, medium, lighter, lightest] to the bucketed greyscale values from 0 -> 255
+    
+    Keyword Arguments
+    
+    colors_repeated = will repeat the array of colors passed in n times (default = 1)
+    """
+    colors = colors * colors_repeated
     img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     
     grayscale_buckets = img
-    # Convert 0 -> 255 to 0 -> 3
     bucket_segments = 255 / len(colors)
     grayscale_buckets = np.rint(np.divide(grayscale_buckets, bucket_segments))
     grayscale_buckets.astype(np.uint8)
@@ -29,3 +28,39 @@ def gameboy(img):
     color_buckets = color_buckets.astype(np.uint8)
 
     return color_buckets
+
+
+def theme_gameboy(img):
+    colors = [
+        [15, 56, 15], # Darkest
+        [48, 98, 48], # Darker
+        [139, 172, 15], # Light 
+        [155, 188, 15], # Lightest
+    ]
+    return theme_method_colors_by_brightness(img, colors)
+
+def theme_beach(img):
+    colors = [
+        [238, 184, 104],
+        [73, 190, 170],
+        [239, 118, 122],
+        [69, 105, 144],
+    ]
+    return theme_method_colors_by_brightness(img, colors)
+
+def theme_deep_beach(img):
+    colors = [
+        [238, 184, 104],
+        [73, 190, 170],
+        [239, 118, 122],
+        [69, 105, 144],
+    ]
+    return theme_method_colors_by_brightness(img, colors, colors_repeated=2)
+
+def theme_black_and_white(img):
+    colors = [
+        [0,0,0],
+        [127,127,127],
+        [255,255,255],
+    ]
+    return theme_method_colors_by_brightness(img, colors)
