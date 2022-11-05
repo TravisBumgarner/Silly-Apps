4d51769cb1d599379c5b938d93ed89eec86382bd
Travis Bumgarner <travis.bumgarner@gmail.com>
2022-11-05 12:37:28 -0600

    added try catch

--- main.py
+++ main.py
@@ -24,14 +24,20 @@ for i_src in range(0,4):
 
 
 with pyvirtualcam.Camera(width=WIDTH, height=HEIGHT, fps=30) as cam:
-    print(f'Using virtual camera: {cam.device}')
-    while True:
-        img = vs.read()
-        img = imutils.resize(img, width=WIDTH, height=HEIGHT)
-        img = imutils.opencv2matplotlib(img) 
+        print(f'Using virtual camera: {cam.device}')
+        while True:
+            try:
+                img = vs.read()
+                img = imutils.resize(img, width=WIDTH, height=HEIGHT)
+                img = imutils.opencv2matplotlib(img) 
 
-        img = gameboy(img)
-        
-        cv2.cvtColor(img, cv2.COLOR_RGB2BGR) # My Webcam sends RGB but we need BGR. 
-        cam.send(img)
-        cam.sleep_until_next_frame()
+                img = gameboy(img)
+                
+                cv2.cvtColor(img, cv2.COLOR_RGB2BGR) # My Webcam sends RGB but we need BGR. 
+                cam.send(img)
+                cam.sleep_until_next_frame()
+            except ValueError as e:
+                # At One point something was crashing when a frame of 1920x1440 was being passed around 
+                # instead of 1920x1280. Issue is gone. Will leave try catch here for now. 
+                print(e)
+                print('invalid frame size')
